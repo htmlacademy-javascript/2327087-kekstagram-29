f03d314bdbcce03b734e642f81982ad5db416202
Neo <106486790+neo5oo@users.noreply.github.com>
2023-06-26 12:52:27 +0300

    eslint

--- js/functions.js
+++ js/functions.js
@@ -39,13 +39,13 @@ const workingDay = (beginning, end, startMeeting, durationMeeting) => {
     return false;
   }
   return true;
-}
+};
 
 
-console.log(workingDay('08:00', '17:30', '14:00', 90));
-console.log(workingDay('8:0', '10:0', '8:0', 120));
-console.log(workingDay('08:00', '14:30', '14:00', 90));
-console.log(workingDay('14:00', '17:30', '08:0', 90));
-console.log(workingDay('8:00', '17:30', '08:00', 900));
+// console.log(workingDay('08:00', '17:30', '14:00', 90));
+// console.log(workingDay('8:0', '10:0', '8:0', 120));
+// console.log(workingDay('08:00', '14:30', '14:00', 90));
+// console.log(workingDay('14:00', '17:30', '08:0', 90));
+// console.log(workingDay('8:00', '17:30', '08:00', 900));
 
 
