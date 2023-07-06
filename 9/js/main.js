665a00c9b86bb675a4caf08c9151b1168bf5df20
Keks <keks@htmlacademy.ru>
2023-07-06 17:22:07 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,4 @@
+import {getArrPhotos} from './data.js';
+import { renderThumbnails } from './rendering-thumbnails.js';
+
+renderThumbnails(getArrPhotos());
---
+++ 9/js/thumbnails-popup.js
@@ -0,0 +1,26 @@
+import {showPopup} from './popup.js';
+
+//контейнер
+const popup = document.querySelector('.big-picture');
+const commentTemplate = popup.querySelector('.social__comment');
+
+function renderPopup({url, description, likes, comments}) {
+  popup.querySelector('.big-picture__img img').src = url;
+  popup.querySelector('.social__comments').replaceChildren(...comments.map(createComment));
+  popup.querySelector('.likes-count').textContent = likes;
+  popup.querySelector('.social__caption').alt = description;
+
+  showPopup(popup);
+}
+
+function createComment(comments) {
+  const comment = commentTemplate.cloneNode(true);
+
+  comment.querySelector('.social__picture').src = comments.avatar;
+  comment.querySelector('.social__picture').alt = comments.name;
+  comment.querySelector('.social__text').textContent = comments.message;
+
+  return comment;
+}
+
+export default renderPopup;
