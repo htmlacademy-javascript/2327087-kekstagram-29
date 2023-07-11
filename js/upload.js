1fd6624e4c3eb77a0c46ce895fe0a9d803a579be
neo <neo5oo@mail.ru>
2023-07-11 18:03:44 +0300

    Проверка хэш-тегов с помощью PristineJS, стилизованные подсказки

--- js/upload.js
+++ js/upload.js
@@ -1,12 +1,21 @@
 import renderPopup from './upload-popup.js';
+import './pristine-validators.js';
 
 /**
  * @type {HTMLFormElement}
  */
 const form = document.querySelector('.img-upload__form');
 
+// @ts-ignore
+//Инициировали библиотеку
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+});
+
 form.addEventListener('change', onFormChange);
 form.addEventListener('hide', onFormHide, true);
+form.addEventListener('reset', onFormReset);
 
 /**
  * @param {Event & {target: HTMLInputElement}} event
@@ -22,4 +31,7 @@ function onFormChange(event) {
 function onFormHide() {
   form.reset();
 }
- 
+
+function onFormReset() {
+  pristine.reset();
+}
