cfb2c32e996f3d76870e227a6cbb457e2b5b73c7
neo <neo5oo@mail.ru>
2023-07-12 17:07:36 +0300

    Реализовали слайдер

--- js/upload-popup.js
+++ js/upload-popup.js
@@ -1,9 +1,11 @@
+import initEffectSlider from './effect-slider.js';
 import {showPopup} from './popup.js';
 import initScaleControl from './scale-control.js';
 
 const popup = document.querySelector('.img-upload__overlay');
 const preview = popup.querySelector('img');
 const scaleControl = initScaleControl(popup.querySelector('.scale'));
+const effectSlider = initEffectSlider(popup.querySelector('.effect-level'));
 
 /**
  * @param {File} data
@@ -14,6 +16,10 @@ function renderPopup(data) {
 
   scaleControl.on('update',onScaleControlUpdate);
   scaleControl.setValue(100);
+
+  effectSlider.on('update', onEffectSliderUpdate);
+  effectSlider.setEffect('none');
+
   showPopup(popup);
 }
 
@@ -21,7 +27,11 @@ function onScaleControlUpdate() {
   const percent = scaleControl.getValue();
   const ratio = percent / 100;
 
-  preview.style.setProperty('transform', `scale(${ratio})`)
+  preview.style.setProperty('transform', `scale(${ratio})`);
+}
+
+function onEffectSliderUpdate() {
+  preview.style.setProperty('filter', effectSlider.getCssValue());
 }
 
 export default renderPopup;
