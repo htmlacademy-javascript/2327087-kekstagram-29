79903d84d41c1a2086ce15c26ee005346bdef1b0
neo <neo5oo@mail.ru>
2023-07-21 18:23:18 +0300

    Добавили сообщение об ошибке, если не удалось получить данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 import initGallery from './gallery.js';
+import renderStatus from './status.js';
 import './upload.js';
 import { request } from './utils.js';
 
-/**
- * @type {Array<Picture>}
- */
-const data = await request('https://29.javascript.pages.academy/kekstagram/data');
+try {
+  /**
+   * @type {Array<Picture>}
+   */
+  const data = await request('https://29.javascript.pages.academy/kekstagram/data');
 
-initGallery(data);
+  initGallery(data);
+
+} catch (error) {
+  const title = `Ошибка: ${error.message}`;
+  const button = 'Закрыть';
+
+  renderStatus('error', {title, button});
+}
