36f179115b73198d9e396228635845612a6483e7
neo <neo5oo@mail.ru>
2023-07-28 14:10:22 +0300

    Добавим перерисовку миниатюр при переключении фильтров

--- js/gallery.js
+++ js/gallery.js
@@ -12,11 +12,42 @@ const thumbnailTemplate = document.querySelector('#picture');
  * @param {Array<Picture>} data
  */
 function initGallery(data) {
-  // TODO: Сортировка
+  const filter = createFilter(data);
+
   menu.classList.remove('img-filters--inactive');
   menu.addEventListener('click', onMenuClick);
 
+  menu.addEventListener('toggle', (event) => {
+    const selectedButton = /** @type {HTMLButtonElement} */(event.target);
+    const selectedValue = /** @type {FilterType} */(selectedButton.getAttribute('value'));
+
+    renderThumbnails(filter(selectedValue));
+  }, true);
+
   renderThumbnails(data);
+}
+
+/**
+ * @param {Array<Picture>} data
+ * @param {{randomLimit?: number}} options
+ * @returns {(type: FilterType) => Array<Picture>}
+ */
+function createFilter(data, options = {}) {
+  const {randomLimit = 10} = options;
+
+  return (type) => {
+    const items = structuredClone(data);
+
+    if (type === 'random') {
+      return items.sort(() => Math.random() - .5).slice(0, randomLimit);
+    }
+
+    if (type === 'discussed') {
+      return items.sort((a, b) => b.comments.length - a.comments.length);
+    }
+
+    return items;
+  };
 }
 
 /**
